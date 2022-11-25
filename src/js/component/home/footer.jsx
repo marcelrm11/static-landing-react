a638bae42a29db61c67ce77d4487d040292852bd
marcelrm11 <marcelrm11@gmail.com>
2022-11-25 17:12:01 +0000

    first finished version main body

--- src/js/component/home/footer.jsx
+++ src/js/component/home/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer(props) {
     return (
-        <footer className="py-4 bg-dark text-center text-light position-absolute bottom-0 w-100">
+        <footer className="py-4 mt-4 bg-dark text-center text-light w-100">
             <p className="mb-0">© {props.year} {props.text}</p>
         </footer>
     )
--- src/js/component/home/main.jsx
+++ src/js/component/home/main.jsx
@@ -1,10 +1,37 @@
 import React from "react";
 
+import Hero from "./main/hero.jsx";
+import Card from "./main/card.jsx";
+
 function Main() {
+    const hero = {
+        title: "A Warm Welcome!",
+        text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis cumque aperiam deleniti optio qui? Magni, voluptate adipisci repellat culpa nemo id saepe tempore porro cumque necessitatibus deleniti? Fuga, obcaecati ab?",
+        buttonColor: "primary",
+        cta: "Call to action!"
+    }
+    const cards = [];
+    for (let i = 0; i < 4; i++) {
+        cards.push(
+            <Card key={i}
+                imgUrl={"https://picsum.photos/500/325"}
+                title={"Card title"}
+                text= {"Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus modi vel ea delectus minima id fuga officiis soluta recusandae ipsum!"}
+                cta={"Find out more!"}
+            />)
+    }
+
     return (
         <div>
-            <Hero />
-            <Cards />
+            <Hero 
+                title={hero.title}
+                text={hero.text}
+                buttonColor={hero.buttonColor}
+                cta={hero.cta}
+            />
+            <div className="data-card-group d-flex flex-column flex-lg-row align-items-center gap-3 my-3">
+                {cards}
+            </div>
         </div>
     )
 }

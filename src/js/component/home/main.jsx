7a092b8018ffe667d9efd37d497bfac04fd87ce6
marcelrm11 <marcelrm11@gmail.com>
2022-12-02 20:26:29 +0000

    added products db and styled cards with desc, price and pic

--- src/js/component/home/main.jsx
+++ src/js/component/home/main.jsx
@@ -2,24 +2,27 @@ import React from "react";
 
 import Hero from "./main/hero.jsx";
 import Card from "./main/card.jsx";
+import data from "../products.json"
 
 function Main() {
+    const products = data.products;
     const hero = {
         title: "A Warm Welcome!",
         text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis cumque aperiam deleniti optio qui? Magni, voluptate adipisci repellat culpa nemo id saepe tempore porro cumque necessitatibus deleniti? Fuga, obcaecati ab?",
         buttonColor: "primary",
         cta: "Call to action!"
     }
-    const cards = [];
-    for (let i = 0; i < 4; i++) {
-        cards.push(
-            <Card key={i}
-                imgUrl={"https://picsum.photos/500/325"}
-                title={"Card title"}
-                text= {"Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus modi vel ea delectus minima id fuga officiis soluta recusandae ipsum!"}
-                cta={"Find out more!"}
-            />)
-    }
+    const cards = products.map(product => {
+        return (
+            <Card key={product.id}
+                imgUrl={product.images[0]}
+                title={product.title}
+                text= {product.description}
+                cta={"Buy now!"}
+                price={product.price}
+            />
+        )
+    })
 
     return (
         <div>
@@ -29,7 +32,7 @@ function Main() {
                 buttonColor={hero.buttonColor}
                 cta={hero.cta}
             />
-            <div className="data-card-group d-flex flex-column flex-lg-row align-items-center gap-3 my-3">
+            <div className="data-card-group d-flex flex-column flex-md-row justify-content-center align-items-center align-items-md-stretch gap-3 my-3 flex-wrap">
                 {cards}
             </div>
         </div>
--- src/js/component/home/main/card.jsx
+++ src/js/component/home/main/card.jsx
@@ -3,15 +3,16 @@ import React from "react";
 import Button from "../../misc/button.jsx";
 
 function Card(props) {
+    const text = props.text.length > 66 ? props.text.substring(0, 67) + "..." : props.text;
     return (
         <div className="card" style={{maxWidth: "24em"}}>
             <img src={props.imgUrl} className="card-img-top" alt="..." />
             <div className="card-body text-center">
                 <h5 className="card-title">{props.title}</h5>
-                <p className="card-text">{props.text}</p>
-                <hr/>
-                <Button color="primary" label={props.cta} />
+                <p className="card-text">{text}</p>
             </div>
+            <h5 className="price">${props.price}</h5>
+            <Button color="primary" label={props.cta} />
         </div>
     )
 }
